90fb885cb0a7b48631f48759b1ab3c3dfe5f2038
Akash Yadav <akashyadav@Akashs-MacBook-Air.local>
2023-12-30 00:22:21 +0530

    create a middleware and function to handle error

--- api/index.js
+++ api/index.js
@@ -29,6 +29,18 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
 
+// Creating Middleware
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500 ;
+    const message = err.message || 'Internal Server Error' ;
+
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message, 
+    });
+});
 
 
 
