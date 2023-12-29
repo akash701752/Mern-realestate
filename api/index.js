fee2cb6741d6b9499b9baa2dcbf6f215591e0141
Akash Yadav <akashyadav@Akashs-MacBook-Air.local>
2023-12-30 00:10:23 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js' ;
+import authRouter from './routes/auth.routes.js' ;
 
 dotenv.config() ;
 
@@ -16,6 +17,8 @@ mongoose
 
 const app = express();
 
+app.use(express.json()) ;
+
 app.listen(3000, () => {
     console.log('Server is Running on PORT 3000!!!');
 });
@@ -23,4 +26,11 @@ app.listen(3000, () => {
 // Creating API Route
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+
+
+
+
+
 
